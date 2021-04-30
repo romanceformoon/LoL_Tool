c82165a64d8ec37f31c9b3c3aeac17a51fa72a94
romanceformoon <lmwljw98@gmail.com>
2021-05-01 00:14:52 +0900

    release v0.1

---
+++ README.md
@@ -0,0 +1,17 @@
+# LoL Tools
++ A simple League of Legends third-party app made in Python. We can use this app by just running.
+
+# How to Use
++ JUST DOUBLE CLICK .EXE AND RUN LoL.
+
+# Build
+    pyinstaller --onefile main.py
+
+# Functions
++ Auto Accept Games
++ Auto Rune Page Set (op.gg)
++ Auto MultiSearch (fow.kr)
+  
+# References
++ AutoBot_OpenSource (https://github.com/jeaho0613/AutoBot_OpenSource)
++ lcu-driver (https://pypi.org/project/lcu-driver/)
--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from lcu_driver import Connector
 import requests
 from bs4 import BeautifulSoup
 import re, json
+from selenium import webdriver
+import chromedriver_autoinstaller
+from win10toast_click import ToastNotifier 
+import logging
+from PIL import Image
 
+
+logging.basicConfig(
+  format = '%(asctime)s %(message)s',
+  datefmt = '[%p %I:%M:%S]',
+  level = logging.INFO
+)
 
 CURRENT_SUMMONER = '/lol-summoner/v1/current-summoner'
 CURRNET_RUNE_PAGE = '/lol-perks/v1/currentpage'
@@ -10,6 +21,8 @@ RUNE_PAGE = '/lol-perks/v1/pages'
 CURRENT_CHAMPION = '/lol-champ-select/v1/current-champion'
 CURRENT_STATE = '/lol-gameflow/v1/gameflow-phase'
 MATCH_READY = '/lol-matchmaking/v1/ready-check/accept'
+SUMMONER_DATA = '/lol-summoner/v1/summoners/'
+BANPICK_PHASE_DATA = '/lol-champ-select/v1/session'
 
 lol_current_version = requests.get("https://ddragon.leagueoflegends.com/api/versions.json")
 DDRAGON_CHAMPION = requests.get('http://ddragon.leagueoflegends.com/cdn/' + json.loads(lol_current_version.text)[0] + '/data/ko_KR/champion.json').json()
@@ -20,13 +33,61 @@ for champ in DDRAGON_CHAMPION['data']:
 
 # print(CHAMPION_KEY_DICT)
 
-
 connector = Connector()
 
 def parsingRune(parsedStr):
     m = re.search('[0-9][0-9][0-9][0-9][.]png', parsedStr)
 
     return m.group().split('.')[0]
+
+
+def open_image():
+    try:
+        image = Image.open("image/out.png")
+        image.show()
+    except:
+        pass
+
+
+async def fowMultiSearch(connection):
+    summonerList = ""
+    matching = await connection.request('get', BANPICK_PHASE_DATA)
+    matchingData = await matching.json()
+    teamCount = len(matchingData["myTeam"])
+    for i in range(teamCount):
+        summonerId = matchingData["myTeam"][i]["summonerId"]
+        if summonerId != 0:
+            summoners = await connection.request('get', SUMMONER_DATA + str(summonerId))
+            summonerData = await summoners.json()
+            summonerName = summonerData["displayName"]
+            summonerList = summonerList + summonerName + ","
+    # print(summonerList[:-1])
+
+    path = chromedriver_autoinstaller.install(True)
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    fowUrl = "http://fow.kr/multi#" + summonerList[:-1].replace(" ", "")
+    # print(fowUrl)
+
+    driver = webdriver.Chrome(path, options=options)
+        
+    driver.get(fowUrl)
+    driver.implicitly_wait(time_to_wait=10)
+    result = driver.find_element_by_id('multi_result')
+    action = webdriver.ActionChains(driver)
+    action.move_to_element(result).perform()
+    result_png = result.screenshot_as_png
+    with open("image/out.png", "wb") as file:
+        file.write(result_png)
+
+    driver.close()
+    driver.quit()
+    logging.info("멀티서치 완료.")
+    toast = ToastNotifier()
+    toast.show_toast("멀티서치 완료","확인하려면 클릭하세요!", duration=10, threaded=True, icon_path="image/icon.ico", callback_on_click=open_image)
 
 
 def opggParsing(currentChamp):
@@ -64,15 +125,14 @@ def opggParsing(currentChamp):
         # print(runeNum)
         return runeNum
 
-
 # fired when LCU API is ready to be used
 @connector.ready
 async def connect(connection):
-    print('LCU API is ready to be used.')
+    logging.info('LCU API is ready to be used.')
     summoner = await connection.request('get', CURRENT_SUMMONER)
     if summoner.status == 200:
         summonerData = await summoner.json()
-        print(f"환영합니다. 소환사, [{summonerData['displayName']}]")
+        logging.info(f"환영합니다. [{summonerData['displayName']}] 소환사님.")
 
 
 async def runeSetting(connection, currentChamp):
@@ -84,19 +144,19 @@ async def runeSetting(connection, currentChamp):
     
     # 기본 룬페이지가 아닌 것 삭제
     if isDeletable:
-        print("Deleted page", currentPageName, currentPageId)
+        logging.info(f"Deleted rune page {currentPageName}, {currentPageId}")
         await connection.request('delete', RUNE_PAGE + '/' + str(currentPageId))
 
     PerkIds = opggParsing(currentChamp)
     newRune = {"primaryStyleId": PerkIds[0], "subStyleId": PerkIds[1], "selectedPerkIds": PerkIds[2:11], "name": currentChamp}
-    print("Added new page", currentChamp)
+    logging.info(f"Added new rune page {currentChamp}")
     await connection.request('post', RUNE_PAGE, data=newRune)
 
 
 # fired when League Client is closed (or disconnected from websocket)
 @connector.close
 async def disconnect(_):
-    print('클라이언트가 종료되었습니다.')
+    logging.info('클라이언트가 종료되었습니다.')
     await connector.stop()
 
 
@@ -104,29 +164,36 @@ async def disconnect(_):
 # when an update to the user happen (e.g. name change, profile icon change, level, ...) the function will be called
 @connector.ws.register(CURRENT_SUMMONER, event_types=('UPDATE',))
 async def summoner_changed(connection, event):
-    print(f'Summoner [{event.data["displayName"]}] detected.')
+    logging.info(f'Summoner [{event.data["displayName"]}] detected.')
 
 
 @connector.ws.register(CURRENT_STATE, event_types=('UPDATE',))
 async def state_changed(connection, event):
-    print('Now state updated to', event.data)
+    logging.info(f'Now state updated to {event.data}')
+
     if event.data == 'ReadyCheck':
         await connection.request('post', MATCH_READY)
-        print("매칭 수락 완료.")
+        logging.info("매칭 수락 완료.")
+
+    if event.data == 'ChampSelect':
+        await fowMultiSearch(connection)
         
 
 @connector.ws.register(CURRENT_CHAMPION, event_types=('UPDATE','CREATE','DELETE',))
 async def champion_changed(connection, event):
     if int(event.data) > 0:
         currentChamp = CHAMPION_KEY_DICT[str(event.data)]
-        print(f'Current champion updated to {currentChamp} ({str(event.data)})')
+        logging.info(f'Current champion updated to {currentChamp} ({str(event.data)})')
         await runeSetting(connection, currentChamp)
 
-
+'''
 @connector.ws.register('/lol-champ-select/v1/session', event_types=('UPDATE',))
 async def ch_changed(connection, event):
     print(event.data)
     print()
+'''
 
-# starts the connector
+print("LoL Tools v0.1")
+print("Github: https://github.com/romanceformoon/LoL_Tool")
+print()
 connector.start()
